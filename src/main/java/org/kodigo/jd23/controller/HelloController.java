b4df0bafa9b5e3e2fe3575f73c5554c918150ecb
REMR11 <ronaldmejia0jia0211@gmail.com>
2026-03-09 19:43:31 -0600

    Feat:Creacion de endpoint con diferentes annotations

--- src/main/java/org/kodigo/jd23/controller/HelloController.java
+++ src/main/java/org/kodigo/jd23/controller/HelloController.java
@@ -2,6 +2,8 @@ package org.kodigo.jd23.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api")
 public class HelloController {
@@ -11,4 +13,27 @@ public class HelloController {
         return "Hello KodiJava!";
     }
 
+    @GetMapping("/hello2/{nombreAlumno}")
+    public String hello_2(
+            @PathVariable String nombreAlumno
+    ){
+        return "Hello! " + nombreAlumno;
+    }
+
+    @GetMapping("/buscar")
+    public String buscar(
+           @RequestParam String nombre,
+           @RequestParam(defaultValue = "true")boolean activo
+    ){
+        return "Buscado: "+ nombre + ", estudiante activo: "+ activo;
+    }
+
+    @PostMapping("/crear")
+    public String crear(
+            @RequestBody Map<String, Object> body
+            ){
+        String nombre = (String) body.get("nombre");
+        Object edad =  body.get("edad");
+        return "Recibido: "+  nombre + ", edad: "+ edad;
+    }
 }
