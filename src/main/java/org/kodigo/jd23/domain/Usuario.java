f01de4e42b303cbed816d4c055db228cedeac589
REMR11 <ronaldmejia0jia0211@gmail.com>
2026-03-10 19:01:55 -0600

    FIX: cambio de postgresql a h2

---
+++ src/main/java/org/kodigo/jd23/domain/Usuario.java
@@ -0,0 +1,13 @@
+package org.kodigo.jd23.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
