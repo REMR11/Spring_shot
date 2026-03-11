5bb4a88aef546c3d3873fdc48365b609ca36543d
REMR11 <ronaldmejia0jia0211@gmail.com>
2026-03-10 19:24:25 -0600

    Feat: Agrega lombok a los modelos

--- src/main/java/org/kodigo/jd23/domain/Usuario.java
+++ src/main/java/org/kodigo/jd23/domain/Usuario.java
@@ -1,13 +1,32 @@
 package org.kodigo.jd23.domain;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+/**
+ * Hibernate
+ */
 @Entity
 @Table(name = "usuarios")
+
+/**
+ * Lombok
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true,length = 255)
+    private String nombre;
+
+    @Column(nullable = false, unique = true,length = 255)
+    private String email;
 
 }
